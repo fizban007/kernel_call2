6541d84d7dee665650966e647209c9bafedcae68
zijun-huang <yh2847@columbia.edu>
2017-10-11 22:12:44 -0400

    updated 249 for testing

--- kernel/kernel/acceleration.c
+++ kernel/kernel/acceleration.c
@@ -1,8 +1,19 @@
 #include <linux/acceleration.h>
 #include <linux/syscalls.h>
+#include <linux/errno.h>
 
 int set_acceleration(struct dev_acceleration __user *acceleration)
 {
+	printk("in syscall 249 set_acceleration");
+	struct dev_acceleration* k_acceleration;
+	int size = sizeof(acceleration);
+	if(copy_from_user(k_acceleration, acceleration, size)
+		return -EFAULT;
+
+	printk("detected x-axis: %d", k_acceleration->x);
+	printk("detected y-axis: %d", k_acceleration->y);
+	printk("detected z-axis: %d", k_acceleration->z);
+
 	return 0;
 }
 int accevt_destroy(int event_id)
