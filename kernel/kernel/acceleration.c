df88bc35e52142ba17aea49b7adcc0370119c7dd
ZhiyingXu <xzhy.cherry@gmail.com>
2017-10-12 09:13:50 -0400

    add accevt_motion accevt_signal

--- kernel/kernel/acceleration.c
+++ kernel/kernel/acceleration.c
@@ -3,6 +3,30 @@
 #include <linux/errno.h>
 #include <linux/uaccess.h>
 #include <linux/slab.h>
+#include <linux/spinlock.h>
+
+#define WINDOW 20
+static DEFINE_SPINLOCK(acceleration_xyz_lock);
+static DEFINE_SPINLOCK(motion_lock);
+struct acceleration_xyz {
+	struct dev_acceleration *xyz;
+	struct acceleration_xyz *next;
+};
+
+struct acceleration_xyz *head = NULL;
+struct acceleration_xyz *tail = NULL;
+int xyz_len = 0;
+
+struct motion {
+	struct acc_motion *baseline;
+	int e_id;
+	struct motion *next;
+};
+
+struct motion *head_motion = NULL;
+struct motion *tail_motion = NULL;
+int num_event = 0;
+
 int set_acceleration(struct dev_acceleration __user *acceleration)
 {
 	struct dev_acceleration* k_acceleration;
@@ -20,6 +44,18 @@ int set_acceleration(struct dev_acceleration __user *acceleration)
 	kfree(k_acceleration);
 	return 0;
 }
+
+int check_window(struct acceleration_xyz *head, struct motion *ptr) {
+/*	struct acceleration_xyz *sensor_data;
+	if (head == NULL)
+		return 0;
+	sensor_data = head;
+	while(sensor_data != NULL) {
+		
+	}*/
+	return 1;
+	
+}
 int accevt_destroy(int event_id)
 {
 	return 0;
@@ -27,14 +63,103 @@ int accevt_destroy(int event_id)
 
 int accevt_create(struct acc_motion __user *acceleration)
 {
+	struct acc_motion *k_acce;
+	int res;
+	
+	k_acce = kmalloc(sizeof(struct acc_motion), GFP_KERNEL);
+	res = copy_from_user(k_acce, acceleration, 
+				sizeof(struct acc_motion));
+	if (res != 0)
+		return -EFAULT;
+	if(k_acce->frq > WINDOW)
+		k_acce->frq = WINDOW;
+	spin_lock(&motion_lock);
+	num_event++;
+	if (head_motion == NULL) {
+		head_motion = kmalloc(sizeof(struct motion), GFP_KERNEL);
+		head_motion->next = NULL;
+		head_motion->e_id = num_event;
+		head_motion->baseline = k_acce;
+		tail_motion = head_motion;
+	} else {
+		tail_motion->next = kmalloc(sizeof(struct motion), 
+					GFP_KERNEL);
+		tail_motion = tail_motion->next;
+		tail_motion->next = NULL;
+		tail_motion->e_id = num_event;
+		tail_motion->baseline = k_acce;
+	}
+	spin_unlock(&motion_lock);
+	
 	return 0;
 }
+
 int accevt_wait(int event_id)
 {
 	return 0;
 }
 int accevt_signal(struct dev_acceleration __user *acceleration)
 {
+	int res;
+	struct dev_acceleration *k_acce;
+	struct acceleration_xyz *temp;
+	struct motion *ptr;
+
+	k_acce = kmalloc(sizeof(struct dev_acceleration), GFP_KERNEL);
+	res = copy_from_user(k_acce, acceleration,
+				sizeof(struct dev_acceleration));
+	if (res != 0)
+		return -EFAULT;
+	
+	spin_lock(&acceleration_xyz_lock);
+
+	if (head == NULL) {
+		head = kmalloc(sizeof(struct acceleration_xyz), GFP_KERNEL);
+		head->xyz = k_acce;
+		k_acce = NULL;
+		head->next = NULL;
+		tail = head;
+	} else {
+		tail->next = kmalloc(sizeof(struct acceleration_xyz), GFP_KERNEL);
+		tail = tail->next;
+		tail->xyz = k_acce;
+		k_acce = NULL;
+		tail->next = NULL;
+	}
+	xyz_len++;
+
+	/* delete the first acceleration_xyz result and let xyz_len = WINDOW */
+	if (xyz_len > WINDOW) {
+		temp = head;
+		head = head->next;
+		temp->next = NULL;
+		kfree(temp->xyz);
+		kfree(temp);
+		xyz_len = WINDOW;
+	}
+		
+	spin_unlock(&acceleration_xyz_lock);
+
+	/* test result */
+	temp = head;
+	while (temp != NULL) {
+		printk("x=%d,y=%d,z=%d\n",temp->xyz->x,temp->xyz->y,temp->xyz->z);
+		temp = temp->next;
+	}
+
+
+	/* check if some event happen when the motion list is not empty 
+	 * loop each motion struct, call the check_window() function
+	 * return 1 if a event should happen else return 0 
+	 */
+	if (head_motion != NULL) {
+		ptr = head_motion;
+		while(ptr != NULL) {
+			res = check_window(head, ptr);
+			ptr = ptr->next;
+		}
+	}
+	
 	return 0;
 }
 SYSCALL_DEFINE1(accevt_destroy, int, event_id)
